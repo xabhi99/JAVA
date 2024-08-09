0c8de5eee3861fa648430dd5cff20f76a93bdcf6
xabhi99 <124269269+xabhi99@users.noreply.github.com>
2024-08-09 09:34:15 +0530

    Add files via upload
    This is array program in java.

---
+++ Array_Searching.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Array_Searching {
+    public static void main(String[] args) {
+        Scanner S=new Scanner(System.in);
+        int[] arr = {1, 2 ,3, 4, 5 ,6, 8 ,8 ,9 ,10};
+        System.out.println("Welcome to array Serching.....");
+        System.out.println("Enter yhe number you want to search");
+        int num=S.nextInt();
+        boolean isfound = isfound(arr ,num);
+    }
+
+    public static boolean isfound(int[] arr, int num)
+    {
+        return false;
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,15 +1,53 @@
 import java.util.Scanner;
 
-class ADD
+class array
 {
-    public static void main(String[] args){
-        System.out.println("Enter first number=");
-        Scanner S =new Scanner(System.in);
-        int a= S.nextInt();
-        System.out.println("Enter Second number=");
-        int b= S.nextInt();
-        System.out.println("add of two number="+ (a+b));
+    public static void main(String[] args) {
+        //int[] myArr = new int[5];
+//        myArr[0]=10;
+//        myArr[1]=12;
+//        myArr[2]=14;
+//        myArr[3]=14;
+//        myArr[4]=16;
+//        int[] myArr = new int[]{10, 12, 14, 14, 16};
+//        int index =0;
 
+//        System.out.println(myArr[index]);
+//        System.out.println(myArr[1]);
+//        System.out.println(myArr[3]);
+//        System.out.println(myArr[4]);
+//        System.out.println(myArr[0]);
+//        while (index < myArr.length) {
+//            System.out.println(myArr[index]);
+//            index++;
+//        }
+//       int[] marks= new int[10];
+//         int marks[]= new int[10];
+//       marks[0]=12;
+//       marks[1]=23;
+//       marks[2]=32;
+//       marks[3]=32;
+//       for (int i=0;i<4;i++)
+//        System.out.println(marks[i]);
+        Scanner S=new Scanner(System.in);
+        System.out.println("Enter size of array memory=");
+        int size =S.nextInt();
+
+        int number[] =new int[size];
+
+        for (int i=0; i<size;i++)
+        {
+            System.out.println("Enter values=");
+            number[i] =S.nextInt();
+        }
+        int x=S.nextInt();
+        for (int i=0;i<number.length ;i++)
+        {
+            if (number[i]==x)
+            {
+                System.out.println("X found at index=" +i);
+
+            }
+        }
     }
-
 }
