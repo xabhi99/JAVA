1731d7727438009b691c9907a4e8bd4900dc512f
xabhi99 <124269269+xabhi99@users.noreply.github.com>
2024-08-09 09:32:10 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class ADD
+{
+    public static void main(String[] args){
+        System.out.println("Enter first number=");
+        Scanner S =new Scanner(System.in);
+        int a= S.nextInt();
+        System.out.println("Enter Second number=");
+        int b= S.nextInt();
+        System.out.println("add of two number="+ (a+b));
+
+    }
+
+}
